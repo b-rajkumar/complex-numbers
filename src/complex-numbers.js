1d9e67a18cda4b2b80456ee04abec9ee705d4164
Rajkumar <b-rajkumar@users.noreply.github.com>
2023-05-02 19:45:21 +0530

    Implemented display complex number function

--- src/complex-numbers.js
+++ src/complex-numbers.js
@@ -10,4 +10,18 @@ const generateComplexNumber = function({realPart, imaginaryPart}) {
   return {getRealPart, getImaginaryPart};
 };
 
+const displayComplexNumber = function({realPart, imaginaryPart}) {
+  const symbol = (imaginaryPart > 0)? '+' : '';
+
+  if(realPart === 0) {
+    return symbol + imaginaryPart + 'i';
+  }
+
+  if(imaginaryPart === 0) {
+    return realPart + '';
+  }
+
+  return realPart + symbol + imaginaryPart + 'i';
+};
+
 exports.generateComplexNumber = generateComplexNumber;
